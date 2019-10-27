a6ea2238432a08722c9a63d5cfd5a76705abd297
Mike <mjwalshe@gmail.com>
2019-10-27 13:17:04 +0000

    Set up JUnit and add first two tests

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,11 @@
+public class FizzBuzz {
+    public String output(int s) {
+        String result = "";
+        if (s == 3) {
+            result = "Fizz";
+        } else {
+            result = "Buzz";
+        }
+        return result;
+    }
+}
---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,25 @@
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FizzBuzzTest {
+    @Test
+    public void testBuzz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String out = fizzBuzz.output(3);
+
+        Assert.assertThat(out, CoreMatchers.is("Fizz"));
+    }
+
+    @Test
+    public void testFizz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String out = fizzBuzz.output(5);
+
+        Assert.assertThat(out, CoreMatchers.is("Buzz"));
+    }
+}
