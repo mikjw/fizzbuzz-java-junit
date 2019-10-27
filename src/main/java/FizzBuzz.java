3de283fee5718473d66d5b404379b5b075e2986e
Mike <mjwalshe@gmail.com>
2019-10-27 13:29:09 +0000

    Refactor

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,12 +1,11 @@
 public class FizzBuzz {
     public String output(int s) {
         String result = "";
-        if (s % 15 == 0) {
-            result = "FizzBuzz";
-        } else if (s % 3 == 0) {
-            result = "Fizz";
-        } else if (s % 5 == 0) {
-            result = "Buzz";
+        if (s % 3 == 0) {
+            result += "Fizz";
+        }
+        if (s % 5 == 0) {
+            result += "Buzz";
         }
         return result;
     }
